6afa570147cc1af8ebf81c48701db5c962d15706
kruvv <kruvv@mail.ru>
2024-12-03 19:01:21 +0300

    Create app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <!-- <img src="/logo.svg" alt="logo" class="header-logo" /> -->
+    <h2>My Favorites Movies</h2>
+  </header>
+  <div class="movies">{{ movieStore.movies }}</div>
+</template>
+
+<script setup lang="ts">
+import { useMovieStore } from "./stores/MovieStore.ts";
+const movieStore = useMovieStore();
+</script>
+
+<style scoped></style>
