ce87ac16d818ab5ec96f0dff4945a60163fca34b
kruvv <kruvv@mail.ru>
2024-12-06 18:45:12 +0300

    Сторы переделаны на Composition API

--- src/stores/MovieStore.ts
+++ src/stores/MovieStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 type Movie = {
   original_title: string;
   overview: string;
@@ -9,64 +10,97 @@ type Movie = {
 };
 export type MovieType = Partial<Movie>;
 
-export const useMovieStore = defineStore("movieStore", {
-  state: () => ({
-    movies: [
-      // {
-      //   adult: false,
-      //   backdrop_path: "/zVVYFN9OjiSn43RVYrzgCOu7y6r.jpg",
-      //   genre_ids: [18, 9648, 53],
-      //   id: 9613,
-      //   original_language: "en",
-      //   original_title: "Spider",
-      //   overview:
-      //     "A mentally disturbed man takes residence in a halfway house. His mind gradually slips back into the realm created by his illness, where he replays a key part of his childhood.",
-      //   popularity: 16.367,
-      //   poster_path: "/zvm4WuYxTiGkRagRqHUey0meRQL.jpg",
-      //   release_date: "2002-11-06",
-      //   title: "Spider",
-      //   video: false,
-      //   vote_average: 6.64,
-      //   vote_count: 849,
-      // },
-      // {
-      //   adult: false,
-      //   backdrop_path: "/f99HWL82rx25jOiVE5jo5qA6OjF.jpg",
-      //   genre_ids: [18, 35],
-      //   id: 55825,
-      //   original_language: "en",
-      //   original_title: "Spider",
-      //   overview:
-      //     "A young man tries to make things right again in his relationship after he and his girlfriend get in a fight.",
-      //   popularity: 5.481,
-      //   poster_path: "/97jePhTnH2pHBh4nPfH5aapOsX0.jpg",
-      //   release_date: "2007-06-17",
-      //   title: "Spider",
-      //   video: false,
-      //   vote_average: 6.559,
-      //   vote_count: 118,
-      // },
-    ] as MovieType[],
-    activeTab: 2 as number,
-  }),
-  getters: {
-    watchedMovies(): MovieType[] {
-      return this.movies.filter((movie) => movie.isWatched);
-    },
-    totalCountMovies(state) {
-      return state.movies.length;
-    },
-  },
-  actions: {
-    setActiveTab(id: number) {
-      this.activeTab = id;
-    },
-    toggleWatched(id: number) {
-      const idx = this.movies.findIndex((el) => el.id == id);
-      this.movies[idx].isWatched = !this.movies[idx].isWatched;
-    },
-    deleteMovie(id: number) {
-      this.movies = this.movies.filter((el) => el.id != id);
-    },
-  },
+// export const useMovieStore = defineStore("movieStore", {
+//   state: () => ({
+//     movies: [
+//       // {
+//       //   adult: false,
+//       //   backdrop_path: "/zVVYFN9OjiSn43RVYrzgCOu7y6r.jpg",
+//       //   genre_ids: [18, 9648, 53],
+//       //   id: 9613,
+//       //   original_language: "en",
+//       //   original_title: "Spider",
+//       //   overview:
+//       //     "A mentally disturbed man takes residence in a halfway house. His mind gradually slips back into the realm created by his illness, where he replays a key part of his childhood.",
+//       //   popularity: 16.367,
+//       //   poster_path: "/zvm4WuYxTiGkRagRqHUey0meRQL.jpg",
+//       //   release_date: "2002-11-06",
+//       //   title: "Spider",
+//       //   video: false,
+//       //   vote_average: 6.64,
+//       //   vote_count: 849,
+//       // },
+//       // {
+//       //   adult: false,
+//       //   backdrop_path: "/f99HWL82rx25jOiVE5jo5qA6OjF.jpg",
+//       //   genre_ids: [18, 35],
+//       //   id: 55825,
+//       //   original_language: "en",
+//       //   original_title: "Spider",
+//       //   overview:
+//       //     "A young man tries to make things right again in his relationship after he and his girlfriend get in a fight.",
+//       //   popularity: 5.481,
+//       //   poster_path: "/97jePhTnH2pHBh4nPfH5aapOsX0.jpg",
+//       //   release_date: "2007-06-17",
+//       //   title: "Spider",
+//       //   video: false,
+//       //   vote_average: 6.559,
+//       //   vote_count: 118,
+//       // },
+//     ] as MovieType[],
+//     activeTab: 2 as number,
+//   }),
+//   getters: {
+//     watchedMovies(): MovieType[] {
+//       return this.movies.filter((movie) => movie.isWatched);
+//     },
+//     totalCountMovies(state) {
+//       return state.movies.length;
+//     },
+//   },
+//   actions: {
+//     setActiveTab(id: number) {
+//       this.activeTab = id;
+//     },
+//     toggleWatched(id: number) {
+//       const idx = this.movies.findIndex((el) => el.id == id);
+//       this.movies[idx].isWatched = !this.movies[idx].isWatched;
+//     },
+//     deleteMovie(id: number) {
+//       this.movies = this.movies.filter((el) => el.id != id);
+//     },
+//   },
+// });
+//
+export const useMovieStore = defineStore("movieStore", () => {
+  const movies = ref<MovieType[]>([]);
+  const activeTab = ref<Number>(2);
+
+  const watchedMovies = computed(() => {
+    return movies.value.filter((el: MovieType) => el.isWatched);
+  });
+
+  const totalCountMovies = computed(() => {
+    return movies.value.length;
+  });
+
+  const setActiveTab = (id: number) => {
+    activeTab.value = id;
+  };
+  const toggleWatched = (id: number) => {
+    const idx = movies.value.findIndex((el: MovieType) => el.id == id);
+    movies.value[idx].isWatched = !movies.value[idx].isWatched;
+  };
+  const deleteMovie = (id: number) => {
+    movies.value = movies.value.filter((el: MovieType) => el.id != id);
+  };
+  return {
+    movies,
+    activeTab,
+    watchedMovies,
+    totalCountMovies,
+    setActiveTab,
+    toggleWatched,
+    deleteMovie,
+  };
 });
--- src/stores/SearchStore.ts
+++ src/stores/SearchStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { useMovieStore } from "./MovieStore.ts";
 import { MovieType } from "./MovieStore.ts";
+import { ref } from "vue";
 
 const URL_API =
   "https://api.themoviedb.org/3/search/movie?api_key=ad433a5055edbcbe820119c8cadcc933&query=";
@@ -10,28 +11,60 @@ interface SearchMovie {
   loader: Boolean;
 }
 
-export const useSearchStore = defineStore("searchStore", {
-  state: (): SearchMovie => ({
-    movies: [],
-    loader: false,
-  }),
-  actions: {
-    async getMovies(search: string) {
-      this.loader = true;
-      try {
-        const res = await fetch(`${URL_API}${search}`);
-        const data = await res.json();
-        this.movies = data.results;
-      } catch (err) {
-        console.log(err);
-      } finally {
-        this.loader = false;
-      }
-    },
-    addToUserMovies(obj: MovieType) {
-      const movieStore = useMovieStore();
-      movieStore.movies.push({ ...obj, isWatched: false });
-      movieStore.activeTab = 1;
-    },
-  },
+/** Options API*/
+// export const useSearchStore = defineStore("searchStore", {
+//   state: (): SearchMovie => ({
+//     movies: [],
+//     loader: false,
+//   }),
+//   actions: {
+//     async getMovies(search: string) {
+//       this.loader = true;
+//       try {
+//         const res = await fetch(`${URL_API}${search}`);
+//         const data = await res.json();
+//         this.movies = data.results;
+//       } catch (err) {
+//         console.log(err);
+//       } finally {
+//         this.loader = false;
+//       }
+//     },
+//     addToUserMovies(obj: MovieType) {
+//       const movieStore = useMovieStore();
+//       movieStore.movies.push({ ...obj, isWatched: false });
+//       movieStore.activeTab = 1;
+//     },
+//   },
+// });
+
+/** Composition API */
+export const useSearchStore = defineStore("searchStore", () => {
+  const loader = ref(false);
+  const movies = ref([]);
+
+  const getMovies = async (search: string) => {
+    loader.value = true;
+    try {
+      const res = await fetch(`${URL_API}${search}`);
+      const data = await res.json();
+      movies.value = data.results;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      loader.value = false;
+    }
+  };
+  const addToUserMovies = (obj: MovieType) => {
+    const movieStore = useMovieStore();
+    movieStore.movies.push({ ...obj, isWatched: false });
+    movieStore.activeTab = 1;
+  };
+
+  return {
+    loader,
+    movies,
+    getMovies,
+    addToUserMovies,
+  };
 });
